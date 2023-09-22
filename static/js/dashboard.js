3d18ce54c11e85bdec9aa0344a71d9f652a88165
jayanthmahadesh <jayanthmahadesh777@gmail.com>
2023-09-22 09:09:27 +0530

    update in dashboard

---
+++ static/js/dashboard.js
@@ -0,0 +1,20 @@
+const showPopupButton = document.getElementById("showPopup");
+const closePopupButton = document.getElementById("closePopup");
+const popupContainer = document.getElementById("popupContainer");
+const overlay = document.getElementById("overlay");
+showPopupButton.addEventListener("click", () => {
+  popupContainer.style.display = "flex";
+  overlay.style.display = "block";
+});
+
+closePopupButton.addEventListener("click", () => {
+  popupContainer.style.display = "none";
+  overlay.style.display = "none";
+});
+function openChat() {
+  document.getElementById("chatbot").style.width = "250px";
+}
+
+function closeChat() {
+  document.getElementById("chatbot").style.width = "0";
+}
